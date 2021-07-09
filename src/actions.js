fa9c18e08cfae5a6390c5e8498ffd13b704fec07
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-07-09 22:02:21 +0530

    menu driven program📃

---
+++ src/actions.js
@@ -0,0 +1,17 @@
+const addPassword = async () => {
+  console.log("addNewPassword");
+};
+
+const viewAllApps = async () => {
+  console.log("viewAllApps");
+};
+
+const getPassword = async () => {
+  console.log("getPassword");
+};
+
+module.exports = {
+  addPassword,
+  viewAllApps,
+  getPassword,
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 #!/usr/bin/env node
-console.log("Hello Node ❤");
+// @ts-nocheck
+const { prompt, Select } = require("enquirer");
+const { addPassword, viewAllApps, getPassword } = require("./actions");
+
+const getMasterPassword = async () => {
+  const { password } = await prompt([
+    {
+      type: "password",
+      name: "password",
+      message: "Enter your Master Password",
+    },
+  ]);
+  return password;
+};
+
+const menu = async () => {
+  console.log("---------------------------------------------");
+  const choice = await new Select({
+    name: "choice",
+    message: "What do want to do?",
+    choices: ["New Password", "View All Apps", "Get Password", "Quit"],
+  }).run();
+  return choice;
+};
+
+const main = async () => {
+  const masterPassword = await getMasterPassword();
+  if (!masterPassword) return;
+
+  while (true) {
+    const choice = await menu();
+    switch (choice) {
+      case "New Password":
+        await addPassword();
+        break;
+
+      case "View All Apps":
+        await viewAllApps();
+        break;
+
+      case "Get Password":
+        await getPassword();
+        break;
+
+      case "Quit":
+        return console.log("Quit");
+    }
+  }
+};
+
+main();
